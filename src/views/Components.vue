08b9cd0d00493c78e80584f49f49ec5fe9135257
Daniel Chan <danielchan.yes@gmail.com>
2020-08-07 23:46:00 +0800

    updated landing page & logo

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,7 +1,54 @@
 <template>
     <div>
         <hero></hero>
-        <basic-elements></basic-elements>
+
+        <section class="section section-lg">
+      <div class="container">
+        <div class="row row-grid align-items-center">
+          <div class="col-md-6 order-md-2">
+            <img src="img/theme/promo-1.png" class="img-fluid floating" />
+          </div>
+          <div class="col-md-6 order-md-1">
+            <div class="pr-md-5">
+              <icon
+                name="ni ni-settings-gear-65"
+                class="mb-5"
+                size="lg"
+                type="success"
+                shadow
+                rounded
+              ></icon>
+              <h3>Awesome features</h3>
+              <p>
+                The kit comes with three pre-built pages to help you get started faster. You can change
+                the text and images and you're good to go.
+              </p>
+              <ul class="list-unstyled mt-5">
+                <li class="py-2">
+                  <div class="d-flex align-items-center">
+                    <badge type="success" circle class="mr-3" icon="ni ni-settings-gear-65"></badge>
+                    <h6 class="mb-0">Carefully crafted components</h6>
+                  </div>
+                </li>
+                <li class="py-2">
+                  <div class="d-flex align-items-center">
+                    <badge type="success" circle class="mr-3" icon="ni ni-html5"></badge>
+                    <h6 class="mb-0">Amazing page examples</h6>
+                  </div>
+                </li>
+                <li class="py-2">
+                  <div class="d-flex align-items-center">
+                    <badge type="success" circle class="mr-3" icon="ni ni-satisfied"></badge>
+                    <h6 class="mb-0">Super friendly support team</h6>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+        <!-- <basic-elements></basic-elements>
         <inputs></inputs>
         <custom-controls></custom-controls>
         <navigation></navigation>
@@ -9,7 +56,7 @@
         <icons></icons>
         <examples></examples>
         <download-section></download-section>
-        <carousel></carousel>
+        <carousel></carousel> -->
     </div>
 </template>
 <script>
